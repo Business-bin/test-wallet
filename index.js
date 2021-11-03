cfd2c132f239d60b6c3d32be9d0add0c1f5a3434
SungBin_ <bsb4011@naver.com>
2021-11-03 17:22:51 +0900

    지갑 생성, 이더리움 잔액 조회

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const router = require('./routers');
@@ -6,15 +7,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use('/', router);
 
-app.get('/', (req, res) => {
-    console.log("2222222222222222222")
-    res.send('Hello World!')
-});
-
 app.use((req, res, next) => {
     res.status(404).send('요청한 페이지를 찾을 수 없습니다');
 });
-
 
 app.listen(6000, () => {
     console.log('Example app listening on port '+ 6000);
--- routers/api/wallet.js
+++ routers/api/wallet.js
@@ -1,21 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const bip39 = require('bip39');
-const ethers = require('ethers');
-const querystring = require("querystring");
+const Web3 = require('web3');
+const { hdkey } = require('ethereumjs-wallet');
 
-
-router.post('/creatWallet', async (req, res) => {
+// mnemonic 생성
+router.post('/newMnemnic', async (req, res) => {
     try {
-        const password = req.body.password;
-        console.log(password)
         const mnemonic = bip39.generateMnemonic();
         console.log('mnemonic = '+mnemonic)
-        res.send('지갑 생성 성공 '+mnemonic);
+        res.send('mnemonic 생성 성공 '+mnemonic);
     }catch (e) {
-        console.log('지갑 생성 에러');
-        res.send('지갑 생성 에러');
+        console.log('mnemonic 생성 에러');
         console.log(e);
+        res.send('mnemonic 생성 에러');
+    }
+});
+
+// wallet 생성
+router.post('/newWallet', async (req, res) => {
+    try {
+        const password = req.body.password;
+        // const mnemonic = req.body.mnemonic;
+        const mnemonic = bip39.generateMnemonic();
+        const path = "m/44'/60'/0'/0/0";    // 이더리움 경로, 비트코인은 m/44'/0'/0'/0
+
+        const seed = bip39.mnemonicToSeedSync(mnemonic, password);  //  mnemonic, password 로 시드 생성
+        const hdWallet = hdkey.fromMasterSeed(seed);    // 마스터 키,
+        const wallet = hdWallet.derivePath(path).getWallet();
+        const address = "0x" + wallet.getAddress().toString("hex");
+        const privateKey = wallet.getPrivateKey().toString("hex");
+
+        const result = {
+            msg : "지갑생성 생성 성공"
+            , hdWallet
+            , mnemonic
+            , wallet
+            , address
+            , privateKey
+        }
+        res.send(result);
+    }catch (e) {
+        console.log(e);
+        res.send('지갑생성 생성 에러');
+    }
+});
+
+// 지갑 balance 조회
+router.post('/getBalance', async (req, res) => {
+    try {
+        const web3 = new Web3(process.env.eth_con); // infura project id
+        const balance = await web3.eth.getBalance(process.env.eth_address); // wallet address
+        console.log(balance);
+
+        const result = {
+            msg : "balance 조회 성공"
+            , balance
+        }
+        res.send(result);
+    }catch (e) {
+        console.log(e);
+        res.send('조회 에러');
     }
 });
 
